834a0d4661d7462aac97cb1822665cab8d33c536
bollen <david.boll@me.com>
2018-02-22 17:06:24 +0100

    fixed error in landing_page_steps

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -10,9 +10,8 @@ Given("the following articles exists") do |table|
   table.hashes.each do |article|
     Article.create!(article)
   end
+end
 
 Then(/^show me the page$/) do
   save_and_open_page
-  end
-  
 end
