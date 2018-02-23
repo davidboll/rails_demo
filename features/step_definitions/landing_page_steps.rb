9a9b6094a5e86a46b92bded2ab81aabbf66c47dd
Antonella Morittu <morittuantonella@gmail.com>
2018-02-23 14:47:29 +0100

    added author to landing page

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -16,8 +16,6 @@ Then(/^show me the page$/) do
   save_and_open_page
 end
 
-
-
 When("I am on the landing page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit root_path
 end
