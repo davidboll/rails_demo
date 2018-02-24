35fe07b475fe02aedc20cfd2344e85a9d3515d4c
bollen <david.boll@me.com>
2018-02-24 16:49:32 +0100

    Adds validation of e-mail to comments-form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   validates :body, presence: true
   belongs_to :article
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, :unless => :body?, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}
+
 end
