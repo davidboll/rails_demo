c64e5d74d18f60c16ff38d859790153746902b8e
bollen <david.boll@me.com>
2018-02-24 19:28:47 +0100

    Adds the option to leave email blank when you add a comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ApplicationRecord
   validates :body, presence: true
   belongs_to :article
-
-   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, presence: false
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, :allow_blank => true
 end
